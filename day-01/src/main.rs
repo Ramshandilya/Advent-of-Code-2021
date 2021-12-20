8bc00711b61af212f4487f1cd823afda4683d761
Ram Shandilya <ram.shandilya@apple.com>
2021-12-20 12:21:58 -1000

    Add Tests

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,12 +1,19 @@
+use std::fs;
 fn main() {
-    let values: Vec<u32> = include_str!("../input.txt")
-        .lines()
-        .map(|s| s.parse::<u32>().unwrap())
-        .collect();
+    let values = read_file("day-01/input.txt");
 
     println!("PART 1: {}", part_one(&values));
     println!("PART 2: {}", part_two(&values));
 
+}
+
+fn read_file(filename: &str) -> Vec<u32> {
+    let values: Vec<u32> = fs::read_to_string(filename)
+        .expect("File not found")
+        .lines()
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+    values
 }
 
 fn part_one(values: &Vec<u32>) -> usize {
@@ -15,4 +22,21 @@ fn part_one(values: &Vec<u32>) -> usize {
 
 fn part_two(values: &Vec<u32>) -> usize {
     return values.windows(4).filter(|i| i.first() < i.last()).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let values: Vec<u32> = read_file("test_input.txt");
+        assert_eq!(part_one(&values), 7);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let values: Vec<u32> = read_file("test_input.txt");
+        assert_eq!(part_two(&values), 5);
+    }
 }
